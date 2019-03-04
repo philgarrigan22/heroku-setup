5a498eaedc2b8a63d7fda8c20a5fcfe89a3f44b6
philgarrigan22 <philgarrigan@comcast.net>
2019-03-04 16:23:52 -0500

    Add update feature for ingredients

---
+++ curl-scripts/ingredients/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl "http://localhost:4741/ingredients/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --data '{
+    "ingredient": {
+      "name": "'"${NAME}"'",
+      "unit": "'"${UNIT}"'"
+    }
+  }'
+
+echo
