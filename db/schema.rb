6c32ff11708b2372ed338865a2355fdaa98eec97
philgarrigan22 <philgarrigan@comcast.net>
2019-03-05 11:38:32 -0500

    Add Foreign Key
    Add Foreign Key to the Resource that Belongs to the Other Resource
    (Recipe)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_153205) do
+ActiveRecord::Schema.define(version: 2019_03_05_162540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2019_03_05_153205) do
     t.string "unit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "recipe_id"
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -53,6 +55,8 @@ ActiveRecord::Schema.define(version: 2019_03_05_153205) do
     t.date "born_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "doctor_id"
+    t.index ["doctor_id"], name: "index_patients_on_doctor_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -73,4 +77,6 @@ ActiveRecord::Schema.define(version: 2019_03_05_153205) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "ingredients", "recipes"
+  add_foreign_key "patients", "doctors"
 end
