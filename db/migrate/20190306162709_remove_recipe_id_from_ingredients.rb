7bcc82b84020cc7919e8a99a3cade8718e1f8fdc
philgarrigan22 <philgarrigan@comcast.net>
2019-03-06 11:35:38 -0500

    Remove recipe_id from ingredient table

--- db/migrate/20190306162709_remove_recipe_id_from_ingredients.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRecipeIdFromIngredients < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :ingredients, :recipe_id, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_161156) do
+ActiveRecord::Schema.define(version: 2019_03_06_163357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,8 +54,6 @@ ActiveRecord::Schema.define(version: 2019_03_06_161156) do
     t.string "unit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "recipe_id"
-    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
   create_table "meals", force: :cascade do |t|
@@ -100,7 +98,6 @@ ActiveRecord::Schema.define(version: 2019_03_06_161156) do
   add_foreign_key "appointments", "doctors"
   add_foreign_key "appointments", "patients"
   add_foreign_key "examples", "users"
-  add_foreign_key "ingredients", "recipes"
   add_foreign_key "meals", "ingredients"
   add_foreign_key "meals", "recipes"
   add_foreign_key "patients", "doctors"
